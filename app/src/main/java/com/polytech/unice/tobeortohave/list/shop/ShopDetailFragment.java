b7579b100d5a8a1cc27ae75c2409913fbf470b90
Alexandre <alex.hiltcher@gmail.com>
2017-05-01 00:22:55 +0200

    oups

--- app/src/main/java/com/polytech/unice/tobeortohave/list/shop/ShopDetailFragment.java
+++ app/src/main/java/com/polytech/unice/tobeortohave/list/shop/ShopDetailFragment.java
@@ -1,4 +1,4 @@
-package com.polytech.unice.tobeortohave.list.shop.dummy;
+package com.polytech.unice.tobeortohave.list.shop;
 
 import android.content.Context;
 import android.net.Uri;
@@ -7,8 +7,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.polytech.unice.tobeortohave.R;
+import com.polytech.unice.tobeortohave.list.dummy.ShopContent;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,53 +21,38 @@ import com.polytech.unice.tobeortohave.R;
  * create an instance of this fragment.
  */
 public class ShopDetailFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private static int shopId;
     private OnFragmentInteractionListener mListener;
 
     public ShopDetailFragment() {
-        // Required empty public constructor
     }
 
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment ShopDetailFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ShopDetailFragment newInstance(String param1, String param2) {
-        ShopDetailFragment fragment = new ShopDetailFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static ShopDetailFragment newInstance(int shopId) {
+        ShopDetailFragment.shopId = shopId;
+        return new ShopDetailFragment();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shop_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_shop_detail, container, false);
+        ((TextView) view.findViewById(R.id.shop_adress)).setText(String.format("Adresse : %s", ShopContent.ITEM_MAP.get(shopId).adress));
+        ((TextView) view.findViewById(R.id.shop_benef)).setText(String.format("Bénéfices : %s", ShopContent.ITEM_MAP.get(shopId).benefits));
+        ((TextView) view.findViewById(R.id.shop_cost)).setText(String.format("Coûts : %s", ShopContent.ITEM_MAP.get(shopId).cost));
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
