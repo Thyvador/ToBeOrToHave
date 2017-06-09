6e813f01f9120840f466ef338ff4d4c22c8dfe09
Alexandre <alex.hiltcher@gmail.com>
2017-06-09 15:20:35 +0200

    Commit de la fin

--- app/src/main/java/com/polytech/unice/tobeortohave/compare/details/CompareDetailsFragment.java
+++ app/src/main/java/com/polytech/unice/tobeortohave/compare/details/CompareDetailsFragment.java
@@ -10,6 +10,7 @@ import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
